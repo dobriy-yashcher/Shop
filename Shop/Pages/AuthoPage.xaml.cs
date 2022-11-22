55874ccd44d9a8a546f9ce173a11b4094f241a5b
202021 <202021@1220-007-22>
2022-11-22 13:18:44 +0300

    Добавьте файлы проекта.

---
+++ Shop/Pages/AuthoPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shop.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthoPage.xaml
+    /// </summary>
+    public partial class AuthoPage : Page
+    {
+        public AuthoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            EventLogin();
+        }
+
+        public void EventLogin()
+        {
+            try
+            {
+                if ((TxtLogin.Text != "") && (TxtPassword.Password != ""))
+                {
+                    var DataLogin = App.Connection.Authorization.Where(z => z.Login == TxtLogin.Text && z.Password == TxtPassword.Password).FirstOrDefault();
+                    if (DataLogin != null)
+                    {
+                        MessageBox.Show(App.Connection.User.Where(z => z.Id == DataLogin.User).FirstOrDefault().Name);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неправильный логин или пароль");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Заполните поля!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void RegistrationButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RegistrationPage());
+        }
+
+        private void TxtPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                EventLogin();
+        }
+    }
+}
---
+++ Shop/Pages/RegistrationPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shop.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationPage.xaml
+    /// </summary>
+    public partial class RegistrationPage : Page
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ReverseButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void RegistrationButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if ((TxtLogin.Text != "") && (TxtName.Text != "") && (TxtPassword.Text != "") && (TxtRepeatPassword.Text != "") && (CBRole.SelectedItem != null))
+                {
+                    if (App.Connection.Authorization.Where(z => z.Login == TxtLogin.Text).FirstOrDefault() == null)
+                    {
+                        if (TxtPassword.Text == TxtRepeatPassword.Text)
+                        {
+
+                            User NewUser = new User() { Name = TxtName.Text };
+
+                            if (CBRole.SelectedItem == "Admin") NewUser.Role = 2;
+                            else NewUser.Role = 1;
+
+                            App.Connection.User.Add(NewUser);
+                            App.Connection.SaveChanges();
+
+                            Authorization NewAuthorization = new Authorization()
+                            {
+                                Login = TxtLogin.Text,
+                                Password = TxtPassword.Text,
+                                User = App.Connection.User.Last().Id
+                            };
+                            App.Connection.Authorization.Add(NewAuthorization);
+                            App.Connection.SaveChanges();
+
+                            NavigationService.GoBack();
+                            MessageBox.Show("Успешно!");
+                        }
+                        else MessageBox.Show("Пароли не совпали!");
+                    }
+                    else MessageBox.Show("Такой логин уже существует!");
+                }
+                else MessageBox.Show("Заполните поля!");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
